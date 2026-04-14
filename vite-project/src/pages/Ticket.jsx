a8760857da16988588b9e71d83b9818b4caa8e12
Fatii78 <fatii78jj@gmail.com>
2026-04-14 15:12:17 +0300

    css

--- vite-project/src/pages/Ticket.jsx
+++ vite-project/src/pages/Ticket.jsx
@@ -41,16 +41,18 @@ const Ticket = ({ park }) => {
   }
 
   return (
-    <div className="ticket-form">
+    <div style={styles.page}>
+    <div style={styles.box}>
       <h2>Buy Tickets for {park.name}</h2>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} style={styles.form}>
         <input
           type="text"
           name="customerName"
           placeholder="Your Name"
           value={formData.customerName}
           onChange={handleChange}
+          style={styles.input}
         />
 
         <input
@@ -59,6 +61,7 @@ const Ticket = ({ park }) => {
           placeholder="Your Email"
           value={formData.customerEmail}
           onChange={handleChange}
+          style={styles.input}
         />
 
         <input
@@ -67,16 +70,65 @@ const Ticket = ({ park }) => {
           placeholder="quantity"
           value={formData.quantity}
           onChange={handleChange}
+          style={styles.input}
         />
-        <h1>
-          totalprice:${formData.quantity ? park.price * formData.quantity : 0}
-        </h1>
 
-        <button type="submit">Reserve</button>
+        <h3>
+          total price: $
+          {formData.quantity ? park.price * formData.quantity : 0}
+        </h3>
+
+        <button type="submit" style={styles.button}>
+          Reserve
+        </button>
       </form>
+
       {message && <p>{message}</p>}
     </div>
+  </div>
   )
+}
+const styles = {
+  page: {
+    height: "80vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontFamily: "Patrick Hand, cursive",
+  },
+
+  box: {
+    border: "1px solid #2c292b",
+    padding: "20px",
+    textAlign: "center",
+    width: "400px",
+    borderRadius: "5px",
+  },
+
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "10px",
+    fontFamily: "Patrick Hand, cursive",
+
+  },
+
+  input: {
+    padding: "5px",
+        fontFamily: "Patrick Hand, cursive",
+
+  },
+
+  button: {
+  padding: "10px 20px",
+    fontSize: "18px",
+    cursor: "pointer",
+    backgroundColor: "#6b3862",
+    color: "#fff",
+    border: "none",
+    borderRadius: "5px",
+    fontFamily: "Patrick Hand, cursive"
+  },
 }
 
 export default Ticket
